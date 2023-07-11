ebd06aea099016bbcc534cfc9f73fe3f0a050735
100JM <bjm_9310@naver.com>
2023-07-11 15:37:32 +0900

    Button, User list 추가(useState)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UserList';
 
 function App() {
+  const [userList, setUserList] = useState([]);
+
+  const userListEvent = (uName, uAge) => {
+    setUserList((preUserList) => {
+      return [...preUserList, {name : uName, age : uAge, id : Math.random()}];
+    });
+  };
+
   return (
     <div>
-        <AddUser />
+        <AddUser onUserList={userListEvent}/>
+        <UsersList users={userList}/>
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,45 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css"
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
-    
+    const [userName, setUserName] = useState('');
+    const [userAge, setUserAge] = useState('');
+
     const addUserEvent = (e) => {
         e.preventDefault();
+        
+        if(userName.trim().length === 0 || userAge.trim().length === 0){
+            return;
+        }
+
+        if(+userAge < 1){
+            return;
+        }
+
+        props.onUserList(userName, userAge);
+
+        setUserName('');
+        setUserAge('');
+    };
+
+    const userNameChangeEvt = (e) => {       
+        setUserName(e.target.value);
+    };
+
+    const userAgeChangeEvt = (e) => {
+        setUserAge(e.target.value);
     };
 
     return(
         <Card className={classes.input}>
             <form onSubmit={addUserEvent}>
                 <label htmlFor="userName">Name</label>
-                <input type="text" id="userName"/>
+                <input type="text" id="userName" value={userName} onChange={userNameChangeEvt}/>
                 <label htmlFor="userAge">Age</label>
-                <input type="number" id="userAge"/>
-                <button type="submit">Add User</button>
+                <input type="number" id="userAge" value={userAge} onChange={userAgeChangeEvt}/>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     );
